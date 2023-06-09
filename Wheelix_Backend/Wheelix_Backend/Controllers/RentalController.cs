48581b3e425e0f6d6051a845fa62d89751b0f68e
Azurba <joaoh.pimenta.giudice@gmail.com>
2023-06-09 08:56:21 -0600

    add rental controller for add and get rentals

---
+++ Wheelix_Backend/Wheelix_Backend/Controllers/RentalController.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Wheelix_Backend.Model;
+
+namespace Wheelix_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalController : ControllerBase
+    {
+        private readonly WheelixDBContext context; //EF
+        private readonly IDbConnection dbConnection; //Dapper
+
+        public RentalController(WheelixDBContext context, IDbConnection dbConnection)
+        {
+            this.context = context; //EF
+            this.dbConnection = dbConnection; //Dapper)
+        }
+
+        //Dapper
+        [HttpGet]
+        public async Task<ActionResult<List<Rental>>> getAllRentals() {
+            var rentals = await dbConnection.QueryAsync<Rental>("SELECT * FROM Rental");
+            return Ok(rentals.ToList());
+        }
+
+        //EF
+        [HttpPost]
+        public async Task<ActionResult<Rental>> addRental(Rental rental) {
+            context.Rental.Add(rental);
+            await context.SaveChangesAsync();
+            return Ok(rental);
+        }
+    }
+}
